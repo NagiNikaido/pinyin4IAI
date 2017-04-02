70c352f1402354e96eba149d7903e124ce904584
naginikaido <1203794101@qq.com>
2017-04-02 22:21:16 +0800

    main part finished.

--- src/pinyin.py
+++ src/pinyin.py
@@ -1,5 +1,69 @@
-import codecvt,pickle
+from codecvt import *
+from functools import reduce
+from math import log
+import pickle
 
 ### model loading ###
 
-with open("model.data","rb") as fin: _dic = pickle.load(fin)
+with open("model.data","rb") as fin: model = pickle.load(fin)
+with open("multi.data","rb") as fin: multi = pickle.load(fin)
+multi_tc={}
+
+def _add_val(_key,_dict,_delta=1):
+    if _key in _dict: _dict[_key]+=_delta
+    else:  _dict[_key]=_delta
+
+def _update(_dict,_key,_value):
+    if _key in _dict and _value[0]>_dict[_key][0] or not _key in _dict: _dict[_key]=_value
+
+for a,b in multi.items():
+    _add_val((a[0],char2num(extCharList[a[1]].split("_")[0])),multi_tc,b)
+
+### model smoothing ###
+
+def _model_smooth():
+    pass
+
+_model_smooth()
+
+print("model loaded.")
+
+### Viterbi algorithm ###
+def _calc_res(lc,lr,cc,tc):
+    if tc==-1: # NORMAL
+        if lc==-1 or not len(extCharList[lc])>1:
+            return (lr[0] + (float("-inf") if not (lc,cc) in model else log(model[lc,cc])-log(model[-2,lc])),lr[1]+charList[cc])
+        else:
+            _lc=charNum[extCharList[lc].split("_")[0]]
+            return (lr[0] + (float("-inf") if not (_lc,cc) in model or not (lc,cc) in multi else log(model[_lc,cc])-log(model[-2,_lc])+log(multi[lc,cc])-log(multi_tc[lc,cc])),\
+                    lr[1]+charList[cc])
+    else: # with modification
+        if lc==-1 or not len(extCharList[lc])>1:
+            return (lr[0] + (float("-inf") if not (lc,cc) in model or not (lc,tc) in multi else log(model[lc,cc])-log(model[-2,cc])+log(multi[lc,tc])-log(multi_tc[lc,cc])),\
+                    lr[1]+charList[cc])
+        else:
+            _lc=charNum[extCharList[lc].split("_")[0]]
+            return (lr[0] + (float("-inf") if not (_lc,cc) in model or not (lc,tc) in multi else log(model[_lc,cc])-log(model[-2,_lc])+log(multi[lc,tc])-log(multi_tc[lc,cc])),\
+                    lr[1]+charList[cc])
+
+def _calc_res__(lc,lr,cc,tc):
+    if lc!=-1 and len(extCharList[lc])>1: lc=charNum[extCharList[lc].split("_")[0]]
+    return (lr[0] * (0 if not (lc,cc) in model else model[lc,cc]/model[-2,lc]),lr[1]+charList[cc])
+
+def pinyin2str(sth):
+    dp={-1:(0.,"")}
+    for cp in sth.split():
+        current={}
+        for cc in pinyinMap[cp]:
+            if isMulti(num2char(cc)): tc=extCharNum[num2char(cc)+"_"+cp]
+            else: tc=-1
+            for lc,lr in dp.items():
+                tr=_calc_res(lc,lr,cc,tc)
+                if tr[0]!=float("-inf"):
+                    _update(current,max(tc,cc),tr)
+        dp=current
+    return reduce(lambda a,b: a if a[0]>b[0] else b,dp.values())
+
+if __name__ == '__main__':
+    while True:
+        print(pinyin2str(input()))
--- src/trainer.py
+++ src/trainer.py
@@ -5,33 +5,31 @@ import random,pickle
 
 trainSetPath="train_set"
 
-puncs = re.compile(r"\s|\.|\(|\)|"+"|".join(["，","。","、","：","；","？","！","（","）","《","》",
-                         "-","——","·","……","‘","’","“","”","/","\\[","\\]","【","】","\\|","℃"]))
+puncs = re.compile(r"\s|[a-zA-Z]|\.|\(|\)|"+"|".join(["，","。","、","：","；","？","！","（","）","《","》",
+                         "-","——","·","……","‘","’","“","”","/",r"\\","\\[","\\]","【","】","\\|","℃"]))
 
 def _add_val(_key,_dict,_delta=1):
     if _key in _dict: _dict[_key]+=_delta
     else:  _dict[_key]=_delta
 
-def mapper(file_name,dump_pipe):
+def mapper(file_name,dump_pipe,pick_rate = 0.01,learn_rate = 1):
     pairCount={(-2,-1):0,(-1,-1):0}
 
     def analyse(context):
         lc=list(puncs.split(context))
         for cl in lc:
             if len(cl)<=1: continue
-            if random.uniform(0,1)<0.01 and hasMulti(cl):
-#                print(cl)
-                dump_pipe.send(cl)
+            if random.uniform(0,1)<pick_rate and hasMulti(cl): dump_pipe.send(cl)
             last=""
             for ll in cl:
                 if not ll in charList: last=""
                 else:
-                    _add_val((-2,-1),pairCount)
-                    _add_val((-2,charNum[ll]),pairCount)
+                    _add_val((-2,-1),pairCount,learn_rate)
+                    _add_val((-2,charNum[ll]),pairCount,learn_rate)
                     if last!="":
-                        _add_val((-1,-1),pairCount)
-                        _add_val((-1,charNum[ll]),pairCount)
-                        _add_val((charNum[last],charNum[ll]),pairCount)
+                        _add_val((-1,-1),pairCount,learn_rate)
+                        _add_val((-1,charNum[ll]),pairCount,learn_rate)
+                        _add_val((charNum[last],charNum[ll]),pairCount,learn_rate)
                     last=ll
 
     print("processing ",file_name,"... Start time: ", time.clock())
@@ -46,7 +44,7 @@ def mapper(file_name,dump_pipe):
     dump_pipe.close()
     return pairCount
 
-def post_mapper(recv_pipe):
+def post_mapper(recv_pipe,learn_rate = 1):
     pairCount={}
     print(time.clock())
     while True:
@@ -66,20 +64,34 @@ def post_mapper(recv_pipe):
             if not c in extCharList: last=""
             else:
                 if last!="" and (len(c)>1 or len(last)>1):
-                    _add_val((extCharNum[last],extCharNum[c]),pairCount)
+                    _add_val((extCharNum[last],extCharNum[c]),pairCount,learn_rate)
+                if len(c)>1:
+                    _add_val((-1,extCharNum[c]),pairCount,learn_rate)
                 last=c
     recv_pipe.close()
     print(time.clock())
     return pairCount
 
 if __name__ == '__main__':
-    pairCount,pairCountList={(-2,-1):0,(-1,-1):0},[]
-    extPairCount,extPairCountList={},[]
+    try: fin=open("model.data","rb")
+    except FileNotFoundError: pairCount={(-2,-1):0,(-1,-1):0}
+    else: pairCount=pickle.load(fin); fin.close()
+    try: fin=open("multi.data","rb")
+    except FileNotFoundError: extPairCount={}
+    else: extPairCount=pickle.load(fin); fin.close()
+
+    learn_rate = input("learn_rate(1-10000,default 1) = ")
+    pick_rate = input("pick_rate(0.0-1.0,default 0.01) = ")
+    if learn_rate == "" : learn_rate=1
+    else: learn_rate=int(learn_rate)
+    if pick_rate == "": pick_rate=0.01
+    else: pick_rate=float(pick_rate)
+    pairCountList,extPairCountList=[],[]
     pool=multiprocessing.Pool(processes=8)
     for fn in os.listdir(trainSetPath):
         rp,sp=multiprocessing.Pipe(False)
-        pairCountList.append(pool.apply_async(func=mapper,args=(fn,sp)))
-        extPairCountList.append(pool.apply_async(func=post_mapper,args=(rp,)))
+        pairCountList.append(pool.apply_async(func=mapper,args=(fn,sp,pick_rate,learn_rate)))
+        extPairCountList.append(pool.apply_async(func=post_mapper,args=(rp,learn_rate)))
     pool.close();pool.join()
 
     for pc in pairCountList:
